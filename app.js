220d106ea758e24d2ce29e871f5623a631d972f9
yasu-git <yasu.git.33@gmail.com>
2024-05-09 21:36:08 +0900

    create DBUtil & Connection test successful operation

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
+"use strict";
 //expressを使うためのコード
 const express = require('express');
-
 const app = express();
-
-//mariaDBを操作するutil
 const DBUtil = require('./utils/DBUtil');
-
 //publicフォルダーにアクセス
 //cssやimgなどを置く場所
 app.use(express.static('public'));
-
 //記述することによりexpressアプリがurlエンコードされたデータを適切に解析できるようになる。
-app.use(express.urlencoded({extended: false}));
-
-app.get('/index', (req: any, res:any) =>{
-	res.render('index');
+app.use(express.urlencoded({ extended: false }));
+app.get('/index', (req, res) => {
+    res.render('index');
 });
-
 //サーバーを起動するコード
 app.listen(3000, () => {
-	console.log('Server started on port 3000');
+    console.log('Server started on port 3000');
 });
---
+++ utils/DBUtil.ts
@@ -0,0 +1,22 @@
+const { Sequelize } = require('sequelize');
+const config = require('../config/config.json'); //データベース接続設定情報
+
+//環境に応じた設定を取得する（developmentを使用する
+const env = process.env.NODE_ENV || 'development';
+const envConfig = config[env];
+
+const sequelize = new Sequelize(envConfig.database, envConfig.username, envConfig.password, {
+	host: envConfig.host,
+	dialect: envConfig.dialect,
+	logging: false, // ログを表示したくない場合は、falseに設定します
+});
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('MariaDBに正常に接続されました。');
+	})
+	.catch((error:any) => {
+		console.error('MariaDBへ接続中にエラーが発生しました：', error);
+	});
+
